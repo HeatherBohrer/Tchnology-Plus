f53f3852586e760e3a7e18ebbaf84d8c4c5c8ab9
Heather Bohrer <heatherbohrer2@gmail.com>
2017-05-03 00:49:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php include 'view/header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Technology Plus</title>
+    <link rel="stylesheet" type="text/css" href="main.css"/>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	
+</head>
+
+<!-- the body section -->
+<body>
+<div class="container">
+<header><h1>Technology Plus</h1></header>
+
 <div class="container">
 <main>
     <h1>Menu</h1>
